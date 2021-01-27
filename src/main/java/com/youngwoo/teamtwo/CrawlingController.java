87ed9a4880c8276efbf592bf2719c94f9b3db42b
YoungWoo Kim <38308305+kher1128@users.noreply.github.com>
2021-01-27 17:13:04 +0900

    Update CrawlingController.java

--- src/main/java/com/youngwoo/teamtwo/CrawlingController.java
+++ src/main/java/com/youngwoo/teamtwo/CrawlingController.java
@@ -44,6 +44,8 @@ public class CrawlingController {
             String title = targetObj.get("title").toString();
             title = title.replace("<b>", "");
             title = title.replace("</b>", "");
+            title = title.replace("&quot", "");
+            title = title.replace(";", "");
             result.put("title", title);
             list.add(result);
         }
